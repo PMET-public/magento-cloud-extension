0fec68a2b4c772e27d641b8e7f4d3bebce3d158a
Keith Bentrup <keith@keithbentrup.com>
2018-09-19 10:28:27 -0400

    updates

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,14 +1,65 @@
-/*
+const cuEl = document.getElementById('css-url')
 
-
-chrome.storage.sync.get(['key'], function(result) {
-  console.log('Value currently is ' + result.key);
-}); 
-*/
-const cu = document.getElementById('css-url')
-document.getElementById('css-url').addEventListener('click', function () {
-    console.log('MC D Popup')
-    chrome.storage.sync.set({key: value}, function() {
-        console.log('Value is set to ' + value);
+chrome.storage.sync.get(['cssUrls'], function(result) {
+    const cssUrls = result['cssUrls']
+    if (cssUrls === null) return
+    let mostRecent = null
+    Object.keys(cssUrls).forEach([key, obj] => {
+        if (mostRecent === null) {
+            mostRecent = key
+        } else {
+            if (obj.timestamp > cssUrls.mostRecentUrl.timestamp) {
+              mostRecent = key
+            }
+        }
     });
+    cuEl.value = cssUrls.mostRecent.rawUrl
+}); 
+
+function testAndLoadCSS(val) {
+    if (/https:\/\//i.test(val)) {
+        chrome.tabs.executeScript(null, {code: "window.MCExt.loadCSS('" + val + "')"});
+        chrome.storage.sync.set({'css-url': val});
+    } else if (!val) {
+        chrome.tabs.executeScript(null, {code: "window.MCExt.removeCSS()"});
+        chrome.storage.sync.set({'css-url': val});
+    }
+}
+
+cuEl.addEventListener('change', function (ev) {
+    ev.target.value = ev.target.value.trim()
+    testAndLoadCSS(ev.target.value)
 });
+
+$('#css-url-save').click((ev) => {
+    prompt('Name this stylesheet:','Existing value')
+    testAndLoadCSS(cuEl.value)
+})
+
+$('#css-url').autocomplete(
+    {appendTo: '#selector-container',
+    source: [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+      ]}
+    )

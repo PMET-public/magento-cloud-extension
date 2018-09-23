bfce004db2fd0ec07da5d703402932be30bb3f55
Keith Bentrup <keithbentrup@gmail.com>
2018-09-23 17:23:54 -0400

    tabs should not be sortable b/c tab order is not persisted

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -102,10 +102,4 @@ cssUrlSave.click((ev) => {
 
 $( function() {
   var tabs = $( "#tabs" ).tabs();
-  tabs.find( ".ui-tabs-nav" ).sortable({
-    axis: "x",
-    stop: function() {
-      tabs.tabs( "refresh" );
-    }
-  });
 } );

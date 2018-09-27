06fac2638244e9f45e1433daf49d30620adce906
Keith Bentrup <keithbentrup@gmail.com>
2018-09-27 13:55:43 -0400

    add copy to clipboard logic with domain name

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,6 +1,12 @@
 const cssUrlInput = $('#css-url-input'),
   cssUrlSave = $('#css-url-save')
 
+var tabDomain = null;
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  var tabUrl = tabs[0].url;
+  tabDomain = tabUrl.substring(0,tabUrl.indexOf('/', 8))
+});
+
 function getRawUrl(url) {
   const rawUrl = url.replace(/(https:\/\/github.com.*\/)blob\//,'$1raw\/');
   return rawUrl
@@ -102,4 +108,14 @@ cssUrlSave.click((ev) => {
 
 $( function() {
   var tabs = $( "#tabs" ).tabs();
+  $('.cli-cmd').each(function () {
+    const cmdInput = this;
+    $(this).val($(this).val().replace('{{url}}', tabDomain))
+      .next('.mdi-content-copy')
+      .click(function () {
+        cmdInput.focus()
+        cmdInput.select()
+        document.execCommand('copy')
+      })
+  })
 } );

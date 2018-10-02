24f88732aaaa6019791ba9d69696db6a6345f5d2
Keith Bentrup <keithbentrup@gmail.com>
2018-10-02 11:22:40 -0400

    add password dialog key up and close handler

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -114,7 +114,34 @@ $( function() {
     autoOpen: false,
     modal: true,
     draggable: false,
-    resizable: false
+    resizable: false,
+    dialogClass: "no-close",
+    buttons: [
+      {
+        text: "Ok",
+        click: function() {
+          $( this ).dialog( "close" )
+          const jInput = $('#password-cli-cmd')
+          jInput.val(jInput.val().replace('password=[^ ]+', 'password=' + $('#password-dialog-input').val().trim()))
+          input = jInput[0]
+          input.focus()
+          input.select()
+          document.execCommand('copy')
+        }
+      }
+    ]
+  })
+  $('#password-dialog-input').on('keyup', function () {
+    const errorClass = 'has-error'
+    const passwordVal = $(this).val().replace(/[^A-z0-9]/g,'')
+    $(this).val(passwordVal)
+    if (!(/^[A-z0-9]{7,}$/.test(passwordVal) && /[0-9]/.test(passwordVal) && /[A-z]/.test(passwordVal) )) {
+      $(this).addClass(errorClass)
+      $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
+    } else {
+      $(this).removeClass(errorClass)
+      $('#password-dialog ~ .ui-dialog-buttonpane button').button('enable')
+    }
   })
   var tabs = $('#tabs').tabs()
   $('.cli-cmd').each(function () {

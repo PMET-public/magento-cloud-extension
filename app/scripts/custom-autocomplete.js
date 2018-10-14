fcab2047cdd63535dc1de56cd551eaf2b1225c1e
Keith Bentrup <keithbentrup@gmail.com>
2018-10-14 09:05:54 -0400

    customize autocomplete to allow decoration of separator menu items

---
+++ app/scripts/custom-autocomplete.js
@@ -0,0 +1,13 @@
+$.widget( "custom.advAutocomplete", $.ui.autocomplete, {
+  _renderItem: function (ul, item) {
+    var jItem = this._super(ul, item)
+    if (/^placeholder/.test(item.value)) {
+      jItem.addClass('ui-menu-placeholder-item')
+    }
+    var url = item.value.replace(/.*?(https:\/\/)/, '$1')
+    if (/^https/.test(url)) {
+      jItem.find('div').append('<a target="_blank" href="' + url + '"><span class="mdi mdi-open-in-new"></span></a>')
+    }
+    return jItem
+  }
+});

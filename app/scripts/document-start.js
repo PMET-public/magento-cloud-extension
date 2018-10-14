899720b2b3cd0ecba25dab166e8e5092c0bc7e7b
Keith Bentrup <keithbentrup@gmail.com>
2018-10-14 09:05:08 -0400

    separate lib code from code to run at start

--- app/scripts/document-start.js
+++ app/scripts/document-start.js
@@ -1,36 +1,5 @@
 'use strict'
 
-console.log('MC Demo Content script')
+console.log('MCE: document-start.js running')
 
-const MCExt = {
-  loadCSS: function (url) {
-    url = url.replace(/gist.githubusercontent.com|raw.githubusercontent.com|raw.githubusercontent.com/i,'rawgit.com')
-    $('#css-url').remove()
-    $('<link>', {
-      id: 'css-url',
-      rel: 'stylesheet',
-      type: 'text/css',
-      href: url
-    }).appendTo('head');
-  },
-  removeCSS: function () {
-    $('#css-url').remove()
-  }
-
-}
-
-function getDomain(url) {
-  return getOrigin(url).replace(/.*\/\//,'').replace(/:.*/,'')
-}
-
-function getOrigin(url) {
-  return url.substring(0,url.indexOf('/', 8))
-}
-
-// chrome.storage.sync.get(['cssUrls'], function(result) {
-//   const cssUrls = result['cssUrls'] || {}
-//   const domain = getDomain(location.href)
-//   if (cssUrls[domain] && cssUrls[domain].rawUrl) {
-//     MCExt.loadCSS(cssUrls[domain].rawUrl)
-//   }
-// }); 
+MCExt.loadSavedCSS()
---
+++ app/scripts/lib.js
@@ -0,0 +1,54 @@
+console.log('MCE: lib.js running')
+
+var MCExt = {
+  loadCSS: function (url, cacheBustingEnabled = false) {
+    if (cacheBustingEnabled) {
+      // add dynamic query parameter
+      url += '?ts=' + (new Date() / 1)
+      // github needs more than dynamic query parameter
+      if (/\/\/github.com/.test(url)) {
+        // find latest commit and make request directly
+        
+      }
+    } 
+    // if url is a github.com url, use raw github instead of API to avoid limits
+    url = url.replace(/\/\/github.com(.*?)\/blob\/(.*)/, '//raw.githubusercontent.com$1/$2')
+    this.removeCSS()
+    fetch(url).then(function (response) {
+      return response.text()
+    }).then(function (txt) {
+      const styleEl = document.createElement('style')
+      styleEl.id = 'css-url'
+      styleEl.innerText = txt
+      document.head.appendChild(styleEl)
+    })
+  },
+  removeCSS: function () {
+    const el = document.querySelector('#css-url')
+    if (el) {
+      el.remove()
+    }
+  },
+  loadSavedCSS: function () {
+    chrome.storage.local.get(['isCssInjectorOn', 'cssUrls'], result => {
+      if (result['isCssInjectorOn']) {
+        const cssUrls = result['cssUrls'] || {}
+        const domain = MCExt.getDomain(location.href)
+        if (cssUrls[domain] && cssUrls[domain].rawUrl) {
+          MCExt.loadCSS(cssUrls[domain].rawUrl)
+        }
+      }
+    })
+  },
+  loadInputCSS: function () {
+    chrome.storage.local.get(['inputCSS'], result => {
+
+    })
+  },
+  getDomain: function (url) {
+    return this.getOrigin(url).replace(/.*\/\//,'').replace(/:.*/,'')
+  },
+  getOrigin: function (url) {
+    return url.substring(0,url.indexOf('/', 8))
+  }
+}

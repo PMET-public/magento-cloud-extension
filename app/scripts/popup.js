5ffe19f966bab24ad8f5671b73fd798326fbaae7
Keith Bentrup <keithbentrup@gmail.com>
2018-10-31 08:54:04 -0400

    split up js files for css injector

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,9 +1,3 @@
-var tabDomain = null;
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var tabUrl = tabs[0].url;
-  tabDomain = tabUrl.substring(0,tabUrl.indexOf('/', 8))
-});
-
 $(function() {
   var tabs = $('#tabs').tabs()
 })

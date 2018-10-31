cc512b6da2b3198b63235ab60da4a8d15abb1f77
Keith Bentrup <keithbentrup@gmail.com>
2018-10-31 12:19:08 -0400

    add copy to clipboard function plus other cleanup

--- app/scripts/commands.js
+++ app/scripts/commands.js
@@ -1,24 +1,40 @@
+function isPasswordValid(passwordVal) {
+  return /^[A-z0-9]{7,}$/.test(passwordVal) && /[0-9]/.test(passwordVal) && /[A-z]/.test(passwordVal)
+}
+
+function copyToClipboard(el) {
+  const copyClass = 'copied-to-clipboard-alert'
+  const jCopyEl = $(el)
+    .focus()
+    .select()
+    .parent()
+    .append('<span class="' + copyClass + '">copied!</span>')
+    .find('.' + copyClass)
+    .fadeOut(1000, function () {
+      $(this).remove()
+    })
+  document.execCommand('copy')
+
+}
+
 $(function () {
-  $('#accordion').accordion({
-    active: 1
+  $('#commands-accordion').accordion({
+    active: 1,
+    collapsible: true
   })
   $('#password-dialog').dialog({
     autoOpen: false,
     modal: true,
     draggable: false,
     resizable: false,
-    dialogClass: 'no-close',
     buttons: [
       {
         text: 'Ok',
-        click: function() {
+        click: function () {
           $( this ).dialog('close')
-          const jInput = $('#password-cli-cmd')
-          jInput.val(jInput.val().replace('password=[^ ]+', 'password=' + $('#password-dialog-input').val().trim()))
-          input = jInput[0]
-          input.focus()
-          input.select()
-          document.execCommand('copy')
+          const jCmdInput = $('#password-cli-cmd')
+          jCmdInput.val(jCmdInput.val().replace(/password=[^ ]+/, 'password=' + $('#password-dialog-input').val().trim()))
+          copyToClipboard(jCmdInput)
         }
       }
     ]
@@ -27,25 +43,26 @@ $(function () {
     const errorClass = 'has-error'
     const passwordVal = $(this).val().replace(/[^A-z0-9]/g,'')
     $(this).val(passwordVal)
-    if (!(/^[A-z0-9]{7,}$/.test(passwordVal) && /[0-9]/.test(passwordVal) && /[A-z]/.test(passwordVal) )) {
-      $(this).addClass(errorClass)
-      $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
-    } else {
+    if (isPasswordValid(passwordVal)) {
       $(this).removeClass(errorClass)
       $('#password-dialog ~ .ui-dialog-buttonpane button').button('enable')
+    } else {
+      $(this).addClass(errorClass)
+      $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
     }
   })
   $('.cli-cmd').each(function () {
-    const cmdInput = this
-    $(this).val($(this).val().replace('{{url}}', tabDomain))
+    const jCmdInput = $(this)
+    jCmdInput.val(jCmdInput.val().replace('{{url}}', tabDomain))
       .next('.simple-copy')
-      .click(function () {
-        cmdInput.focus()
-        cmdInput.select()
-        document.execCommand('copy')
+      .click(function (ev) {
+        copyToClipboard(jCmdInput)
       })
   })
   $('#password-prompt').click(function () {
     $('#password-dialog').dialog('open')
+    if (!isPasswordValid($('#password-dialog-input').val())) {
+      $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
+    }
   })
 })

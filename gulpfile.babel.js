fe56af6a4a72bfb4f565bcc77c16555521360c90
Keith Bentrup <keithbentrup@gmail.com>
2018-11-02 12:15:22 -0400

    update tasks for new workflow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -22,26 +22,25 @@ const imageDownloader = [
 ]
 
 const mcmExt = [
-  'app/scripts/custom-autocomplete.js',
-  'app/scripts/lib-for-extension.js',
-  'app/scripts/css-injector/*.js',
-  'app/scripts/commands.js',
-  'app/scripts/popup.js'
+  'app/scripts/popup/custom-autocomplete.js',
+  'app/scripts/lib/lib.js',
+  'app/scripts/popup/css-injector/*.js',
+  'app/scripts/popup/commands.js',
+  'app/scripts/popup/popup.js'
 ]
 
-gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    'app/_locales/**',
-    '!app/*.json',
-    '!app/*.html',
-    '!app/styles.scss',
-    'app/image-downloader/**/*'
-  ], {
-    base: 'app',
-    dot: true
-  }).pipe(gulp.dest('dist'));
-});
+const contentScripts = [
+  'app/scripts/content/lib-for-document.js',
+  'app/scripts/content/document-start.js'
+]
+
+const distBackgroundScripts = [
+  'app/scripts/lib/lib.js',
+  'app/scripts/background/my-background.js',
+  'app/image-downloader/scripts/defaults.js'
+]
+
+const devBackgroundScripts = ['app/crx-hotreload/hot-reload.js', ...distBackgroundScripts]
 
 function lint(files, options) {
   return () => {
@@ -51,29 +50,75 @@ function lint(files, options) {
   };
 }
 
-function concatWithSourceMap(srcs, outputFile) {
+function processJS(srcs, opts) {
+  const defaultOpts = {
+    sourcemaps: 1, 
+    concat: 1, 
+    minify: 0, 
+    uglify: 0, 
+    dest: 'dist/scripts',
+    file: 'out.js'
+  }
+  const combinedOpts = Object.assign(defaultOpts, opts)
   return gulp.src(srcs)
-    .pipe($.sourcemaps.init())
-    .pipe($.concat(outputFile))
-    .pipe(gulp.dest("app/combined"))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest("app/combined"));
+    .pipe($.if(combinedOpts.sourcemaps, $.sourcemaps.init()))
+    .pipe($.if(combinedOpts.concat, $.concat(combinedOpts.file)))
+    .pipe($.if(combinedOpts.minify, $.minify({noSource: true, ext: {min: '.js'}})))
+    .pipe($.if(combinedOpts.uglify, $.uglify()))
+    .pipe(gulp.dest(combinedOpts.dest))
+    .pipe($.if(combinedOpts.sourcemaps, $.sourcemaps.write()))
+    .pipe(gulp.dest(combinedOpts.dest));
 }
+
+gulp.task('copy-remaining-to-dist', () => {
+  return gulp.src([
+    'app/manifest.json',
+    'app/_locales/**',
+    'app/image-downloader/**/*'
+  ], {
+    base: 'app',
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
 
 gulp.task('lint', lint(mcmExt, {
   env: {es6: true}
 }));
 
-gulp.task('js-jquery-deps', () => {
-  return concatWithSourceMap(jqueryDeps, '1-js-jquery-deps.js')
+gulp.task('dev-js', () => {
+  processJS(devBackgroundScripts, {
+    file: 'background/main.processed.js',
+    sourcemaps: 1,
+    minify: 0
+  })
+  processJS(contentScripts, {
+    file: 'content/main.processed.js',
+    sourcemaps: 1,
+    minify: 0
+  })
+  processJS([...jqueryDeps, ...imageDownloader, ...mcmExt], {
+    file: 'popup/main.processed.js',
+    sourcemaps: 1,
+    minify: 0
+  })
 })
 
-gulp.task('js-image-downloader', () => {
-  return concatWithSourceMap(imageDownloader, '2-js-image-downloader.js')
-})
-
-gulp.task('js-mcm-ext', () => {
-  return concatWithSourceMap(mcmExt, '3-js-mcm-ext.js')
+gulp.task('dist-js', () => {
+  processJS(distBackgroundScripts, {
+    file: 'background/main.processed.js',
+    sourcemaps: 0,
+    minify: 1
+  })
+  processJS(contentScripts, {
+    file: 'content/main.processed.js',
+    sourcemaps: 0,
+    minify: 1
+  })
+  processJS([...jqueryDeps, ...imageDownloader, ...mcmExt], {
+    file: 'popup/main.processed.js',
+    sourcemaps: 0,
+    minify: 1
+  })
 })
 
 gulp.task('images', () => {
@@ -81,8 +126,7 @@ gulp.task('images', () => {
     .pipe($.if($.if.isFile, $.cache($.imagemin({
       progressive: true,
       interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
+      // don't remove IDs from SVGs, they are often used as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
     }))
     .on('error', function (err) {
@@ -102,44 +146,26 @@ gulp.task('styles', () => {
       includePaths: ['.']
     }).on('error', $.sass.logError))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('app/styles'));
+    .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('html', () => {
   return gulp.src('app/html/popup.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.css', $.cleanCss({compatibility: '*'})))
-    .pipe($.if('*.html', $.fileInclude()))
-    .pipe($.if('*.html', $.htmlmin({
+    .pipe($.fileInclude())
+    .pipe($.htmlmin({
       collapseWhitespace: true,
       minifyCSS: true,
       minifyJS: true,
       removeComments: true
-    })))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('chromeManifest', () => {
-  return gulp.src('app/manifest.json')
-    .pipe($.chromeManifest({
-      buildnumber: true,
-      background: {
-        target: 'scripts/background.js',
-        exclude: [
-          'crx-hotreload/hot-reload.js'
-        ]
-      }
-  }))
-  .pipe($.if('*.css', $.cleanCss({compatibility: '*'})))
-  .pipe(gulp.dest('dist'));
+    }))
+    .pipe(gulp.dest('dist/html/'));
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('watch', ['lint', 'styles', 'js-jquery-deps', 'js-image-downloader', 'js-mcm-ext'], () => {
-  gulp.watch('app/scripts/**/*.js', ['lint']);
+gulp.task('watch', ['lint', 'styles', 'dev-js'], () => {
+  gulp.watch('app/scripts/**/*.js', ['lint', 'dev-js']);
   gulp.watch('app/styles.scss/**/*.scss', ['styles']);
-  gulp.watch(mcmExt, ['js-mcm-ext']);
 });
 
 gulp.task('wiredep', () => {
@@ -150,17 +176,19 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
+gulp.task('build', cb => {
+  runSequence(['lint', 'dev-js', 'styles', 'html', 'images'], 'copy-remaining-to-dist', cb);
+});
+
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
-  return gulp.src('dist/**')
-      .pipe($.zip('mcm-chrome-ext-' + manifest.version + '.zip'))
-      .pipe(gulp.dest('package'));
+  return gulp.src([
+      'dist/**'
+    ])
+    .pipe($.zip('mcm-chrome-ext-' + manifest.version + '.zip'))
+    .pipe(gulp.dest('package'));
 });
 
-gulp.task('build', cb => {
-  runSequence(['lint', 'chromeManifest', 'html', 'images', 'extras'], cb);
-});
-
-gulp.task('default', ['clean'], cb => {
-  runSequence('build', cb);
+gulp.task('default', cb => {
+  runSequence('clean', 'build', cb);
 });

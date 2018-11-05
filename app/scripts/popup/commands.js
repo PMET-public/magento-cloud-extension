6fd71e11eb1972336ebc5f87212f0d66517219ca
Keith Bentrup <keithbentrup@gmail.com>
2018-11-05 17:19:12 -0500

    support unlocking/creating any admin user

--- app/scripts/popup/commands.js
+++ app/scripts/popup/commands.js
@@ -1,5 +1,20 @@
+function isEmailValid(emailVal) {
+  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(emailVal)
+}
+
+function isUserValid(userVal) {
+  return userVal.length > 1
+}
+
 function isPasswordValid(passwordVal) {
   return /^[A-z0-9]{7,}$/.test(passwordVal) && /[0-9]/.test(passwordVal) && /[A-z]/.test(passwordVal)
+}
+
+function setFormButtonState() {
+  const passed = isEmailValid($('#email-dialog-input').val()) &&
+    isUserValid($('#user-dialog-input').val()) &&
+    isPasswordValid($('#password-dialog-input').val())
+  $('#password-dialog ~ .ui-dialog-buttonpane button').button(passed ? 'enable' : 'disable')
 }
 
 function copyToClipboard(el) {
@@ -18,39 +33,75 @@ function copyToClipboard(el) {
 }
 
 $(function () {
+  
   $('#commands-accordion').accordion({
     active: 1,
     collapsible: true
   })
+  
   $('#password-dialog').dialog({
     autoOpen: false,
     modal: true,
     draggable: false,
     resizable: false,
+    width: 400,
     buttons: [
       {
         text: 'Ok',
         click: function () {
           $( this ).dialog('close')
           const jCmdInput = $('#password-cli-cmd')
-          jCmdInput.val(jCmdInput.val().replace(/password=[^ ]+/, 'password=' + $('#password-dialog-input').val().trim()))
+          jCmdInput.val(
+            jCmdInput.val()
+              .replace(/email=[^ ]+/, 'email=' + $('#email-dialog-input').val())
+              .replace(/user=[^ ]+/, 'user=' + $('#user-dialog-input').val())
+              .replace(/password=[^ ]+/, 'password=' + $('#password-dialog-input').val())
+              .trim()
+            )
           copyToClipboard(jCmdInput)
         }
       }
     ]
   })
-  $('#password-dialog-input').on('keyup', function () {
+  
+  $('#email-dialog-input').on('keyup blur', function () {
+    const errorClass = 'has-error'
+    const emailVal = $(this).val().trim()
+    $(this).val(emailVal)
+    // js email regex from https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression
+    // exclude local valid emails (emails w/o TLDs)
+    if (isEmailValid(emailVal)) {
+      $(this).removeClass(errorClass)
+    } else {
+      $(this).addClass(errorClass)
+    }
+    setFormButtonState()
+  })
+
+  $('#user-dialog-input').on('keyup blur', function () {
+    const errorClass = 'has-error'
+    const userVal = $(this).val().replace(/[^A-z0-9]/g,'')
+    $(this).val(userVal)
+    if (isUserValid(userVal)) {
+      $(this).removeClass(errorClass)
+    } else {
+      $(this).addClass(errorClass)
+    }
+    setFormButtonState()
+  })
+
+  $('#password-dialog-input').on('keyup blur', function () {
     const errorClass = 'has-error'
     const passwordVal = $(this).val().replace(/[^A-z0-9]/g,'')
     $(this).val(passwordVal)
     if (isPasswordValid(passwordVal)) {
       $(this).removeClass(errorClass)
-      $('#password-dialog ~ .ui-dialog-buttonpane button').button('enable')
     } else {
       $(this).addClass(errorClass)
-      $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
     }
+    setFormButtonState()
   })
+  
   $('.cli-cmd').each(function () {
     const jCmdInput = $(this)
     jCmdInput.val(jCmdInput.val().replace('{{url}}', tabDomain))
@@ -59,10 +110,12 @@ $(function () {
         copyToClipboard(jCmdInput)
       })
   })
+  
   $('#password-prompt').click(function () {
     $('#password-dialog').dialog('open')
     if (!isPasswordValid($('#password-dialog-input').val())) {
       $('#password-dialog  ~ .ui-dialog-buttonpane button').button('disable')
     }
   })
+
 })
--- sh-scripts/reset-admin.sh
+++ sh-scripts/reset-admin.sh
@@ -20,5 +20,5 @@ environment=$($CLI_PATH environments -p $project --pipe | \
   awk '{print $1}')
 #echo $environment
 
-$SSH_CMD -p $project -e $environment "php bin/magento admin:user:unlock admin;
-  php bin/magento admin:user:create --admin-user=${user:-admin} --admin-password=${password} --admin-email=${email:-kbentrup@magento.com} --admin-firstname=Admin --admin-lastname=Username"
+$SSH_CMD -p $project -e $environment "php bin/magento admin:user:unlock ${user};
+  php bin/magento admin:user:create --admin-user=${user} --admin-password=${password} --admin-email=${email} --admin-firstname=Admin --admin-lastname=Username"

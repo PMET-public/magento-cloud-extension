a8d7a50c30123e5f3c327bffd9b45cbc2dc3251a
Keith Bentrup <keithbentrup@gmail.com>
2018-11-05 16:22:48 -0500

    update output info; fix interpolation using double quotes

--- sh-scripts/reset-admin.sh
+++ sh-scripts/reset-admin.sh
@@ -7,7 +7,9 @@ if [ -f ${HOME}/.ssh/id_rsa.magento ]; then
 fi
 
 # url is passed via `env url=https://....`
-echo "Attempting ssh login and cmd for: $url"
+green='\033[0;32m'
+no_color='\033[0m'
+printf "\nAttempting ssh login and cmd for:\n${green}${url}${no_color}\n\n"
 
 project=$(echo "$url" | perl -pe "s/.*-//;s/\..*//;")
 #echo $project
@@ -18,5 +20,5 @@ environment=$($CLI_PATH environments -p $project --pipe | \
   awk '{print $1}')
 #echo $environment
 
-$SSH_CMD -p $project -e $environment 'php bin/magento admin:user:unlock admin;
-  php bin/magento admin:user:create --admin-user=admin --admin-password=${password} --admin-email=kbentrup@magento.com --admin-firstname=Admin --admin-lastname=Username'
+$SSH_CMD -p $project -e $environment "php bin/magento admin:user:unlock admin;
+  php bin/magento admin:user:create --admin-user=${user:-admin} --admin-password=${password} --admin-email=${email:-kbentrup@magento.com} --admin-firstname=Admin --admin-lastname=Username"

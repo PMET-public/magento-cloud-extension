34906f87aa104d767c0171be5f5f827a84c45899
Keith Bentrup <keith@keithbentrup.com>
2018-11-14 13:56:21 -0500

    use wget in env or vm b/c may not exist on SC system

--- sh-scripts/cache-warm.sh
+++ sh-scripts/cache-warm.sh
@@ -2,7 +2,10 @@
 wget_url=$(echo "${url}" | perl -pe "s!^(https?://[^/]+).*!\1!")
 wget_domain=$(echo "${wget_url}" | perl -pe "s!https?://!!")
 
+tmp_file=/tmp/$(date '+delete-me-%Y-%m-%d-%H-%M-%S')]
+
 # recursively get admin and store front
-wget -r -l 1 -O delete-me -H --domains="${wget_domain}" "${wget_url}/admin"
-wget -r -l 1 -O delete-me -H --domains="${wget_domain}" "${wget_url}"
-rm delete-me
+printf "\nWarming cache ...\n"
+$ssh_cmd "wget -r -l 1 -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}/admin
+  wget -r -l 1 -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}
+  rm ${tmp_file}"

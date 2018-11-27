f2e1b19b46b8a7071b82af6ca3cfb707d4db15ae
Keith Bentrup <keith@keithbentrup.com>
2018-11-27 14:55:55 -0500

    make sure public key is added

--- sh-scripts/setup-ssh-key.sh
+++ sh-scripts/setup-ssh-key.sh
@@ -4,4 +4,4 @@ if [ ! -f ~/.ssh/id_rsa.magento ]; then
   ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa.magento;
 fi
   
-~/.magento-cloud/bin/magento-cloud ssh-key:add ~/.ssh/id_rsa.magento --yes
+~/.magento-cloud/bin/magento-cloud ssh-key:add ~/.ssh/id_rsa.magento.pub --yes

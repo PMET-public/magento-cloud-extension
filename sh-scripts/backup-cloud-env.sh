d9a38d0a345268ac4a9419a952dd3e4ddb4cc3b1
Keith Bentrup <keith@keithbentrup.com>
2018-11-28 20:26:13 -0500

    add initial cloud backup and recreate scripts

---
+++ sh-scripts/backup-cloud-env.sh
@@ -0,0 +1,46 @@
+printf "\nBacking up env ...\n"
+
+tar_file=/tmp/$(date "+%Y-%m-%d-%H-%M")-${project}-${environment}-backup.tar
+tmp_git_dir="/tmp/delete-me-${environment}"
+
+$ssh_cmd "sqlfile=\$(php ${home_dir}/bin/magento setup:backup --db | sed -n \"s/.*path: //p\")
+
+# replace specific host name with token placeholder
+perl -i -pe \"\\\$c+=s!${simplified_url}!{{REPLACEMENT_BASE_URL}}!g; 
+  END{print \\\"\n\\\$c host name replacements\n\\\"}\" \$sqlfile
+
+# find full paths of imported images and create tar file
+mysql main -sN -h database.internal -e '# all paths of products added after a certain date
+select cpemg.value from 
+  catalog_product_entity cpe, 
+  catalog_product_entity_media_gallery cpemg, 
+  catalog_product_entity_media_gallery_value_to_entity cpemgvte
+where 
+  cpemgvte.row_id = cpe.row_id AND
+  cpemgvte.value_id = cpemg.value_id AND
+  updated_at > 
+# find date of first product plus 30 min
+(select date_add(min(updated_at), interval 30 minute) from 
+  catalog_product_entity cpe, 
+  catalog_product_entity_media_gallery cpemg, 
+  catalog_product_entity_media_gallery_value_to_entity cpemgvte
+where 
+  cpemgvte.row_id = cpe.row_id AND
+  cpemgvte.value_id = cpemg.value_id
+order by updated_at asc)' 2> /dev/null | \
+  sed 's/^/pub\\/media\\/catalog\\/product/' | \
+  tar -cf ${tar_file} --files-from -
+
+# add sql file and other files needed to recreate project/environment
+tar --ignore-failed-read -rf ${tar_file} \$sqlfile .gitignore composer.json composer.lock .magento.app.yaml pub/media/gene-cms pub/media/wysiwyg pub/media/ThemeCustomizer
+"
+
+mkdir -p "${backups_dir}"
+$scp_cmd:$tar_file "${backups_dir}"
+
+# still not done b/c need .magento/services.yml & .magento/routes.yml but they do not exist on the remote cloud filesystem
+# so grab them from the env's repo
+rm -rf "${tmp_git_dir}" # ensure tmp_git_dir doesn't exist from a previously aborted cmd 
+git clone --branch "${environment}" $(${cli_path} project:info -p "${project}" git) "${tmp_git_dir}"
+tar -C "${tmp_git_dir}" -rf "${backups_dir}/${tar_file#/tmp/}" .magento
+rm -rf "${tmp_git_dir}"
---
+++ sh-scripts/create-new-cloud-env-from-backup.sh
@@ -0,0 +1,59 @@
+printf "\nCreating new cloud env from backup ...\n"
+
+# prompt user for tar file and project to create new env on
+menu_height=20
+menu_width=70
+num_visible_choices=10
+backtitle="Creating new cloud env from backup ..."
+
+tar_files=($(find "${backups_dir}" -name "*.tar" 2>/dev/null | perl -pe 's!.*/backups/!!' | cat -n))
+if [[ ${#tar_files[@]} -lt 1 ]]; then
+  printf "\n${red}No backups found in "${backups_dir}".${no_color}\n\n" && exit
+fi
+
+# must use "declare -a" b/c some array items are quoted strings with spaces
+declare -a "projects=($(${cli_path} projects --format=tsv | perl -pe 's/https:.*//' | sed '1d'))"
+if [[ ${#projects[@]} -lt 1 ]]; then
+  printf "\n${red}No projects found. Is your Magento Cloud CLI installed and logged in?${no_color}\n\n" && exit
+fi
+
+selections=$(dialog --clear \
+  --backtitle "${backtitle}" \
+  --title "Your Backup(s)" \
+  --menu "Choose a backup file to deploy:" \
+  $menu_height $menu_width $num_visible_choices "${tar_files[@]}" \
+  --and-widget \
+  --clear \
+  --backtitle "${backtitle}" \
+  --title "Your Project(s)" \
+  --menu "Choose a project for the new env:" \
+  $menu_height $menu_width $num_visible_choices "${projects[@]}" \
+  2>&1 >/dev/tty)
+clear
+
+read -r tar_file_index project_id <<<"${selections}"
+tar_file="${tar_files[$(( (${tar_file_index} - 1) * 2 + 1))]}"
+tmp_tar_dir=$(echo "${tar_file}" | sed 's/^/\/tmp\//;s/.tar$//')
+git_branch="${tar_file%.tar}"
+
+# extract tar file to tmp dir and make git repo
+mkdir -p "${tmp_tar_dir}"
+tar -xf "${backups_dir}/${tar_file}" -C "${tmp_tar_dir}"
+cd "${tmp_tar_dir}"
+git init
+git remote add cloud $("${cli_path}" project:info -p "${project_id}" git)
+git checkout -b "${git_branch}"
+git add .
+git commit -m "Creating from backup: ${tar_file}"
+git push --set-upstream cloud "${git_branch}"
+
+
+# push to cloud & wait for installation to complete
+
+# send tar file
+
+# extract files
+
+# replace hostname
+
+# restore db

bf4f480df31bbda8b78cec0e8b431352b580dba5
Keith Bentrup <keithbentrup@gmail.com>
2018-11-29 07:56:16 -0500

    still initial WIP script

--- sh-scripts/create-new-cloud-env-from-backup.sh
+++ sh-scripts/create-new-cloud-env-from-backup.sh
@@ -31,29 +31,41 @@ selections=$(dialog --clear \
   2>&1 >/dev/tty)
 clear
 
-read -r tar_file_index project_id <<<"${selections}"
-tar_file="${tar_files[$(( (${tar_file_index} - 1) * 2 + 1))]}"
-tmp_tar_dir=$(echo "${tar_file}" | sed 's/^/\/tmp\//;s/.tar$//')
-git_branch="${tar_file%.tar}"
+read -r tar_file_choice project <<<"${selections}"
+if [[ "${tar_file_choice}" -lt 1 ]]; then
+  exit # must have canceled the 1st menu (not sure why dialog cmd does not abort on its own in this scenario)
+fi
 
-# extract tar file to tmp dir and make git repo
-mkdir -p "${tmp_tar_dir}"
-tar -xf "${backups_dir}/${tar_file}" -C "${tmp_tar_dir}"
-cd "${tmp_tar_dir}"
+tar_file="${tar_files[$(( (${tar_file_choice} - 1) * 2 + 1))]}" # account for menu numbering vs array with labels numbering
+tmp_git_dir=$(echo "${tar_file}" | sed 's/^/\/tmp\//;s/.tar$//')
+environment="${tar_file%.tar}"
+
+# if env already exists in project, then exit with error
+if ${cli_path} environment:info -p "${project}" -e "${environment}" > /dev/null 2>&1; then
+  error Environment "${environment}" on project "${project}" already exists. \
+    Use the delete command to remove it and start over if necessary.
+fi
+
+# extract tar file to tmp dir and make git repo to push to cloud
+rm -rf "${tmp_git_dir}" # ensure tmp_git_dir doesn't exist from a previously aborted cmd
+mkdir -p "${tmp_git_dir}"
+tar -xf "${backups_dir}/${tar_file}" -C "${tmp_git_dir}"
+cd "${tmp_git_dir}"
 git init
-git remote add cloud $("${cli_path}" project:info -p "${project_id}" git)
-git checkout -b "${git_branch}"
+git remote add cloud $("${cli_path}" project:info -p "${project}" git)
+git checkout -b "${environment}"
 git add .
 git commit -m "Creating from backup: ${tar_file}"
-git push --set-upstream cloud "${git_branch}"
+git push --set-upstream cloud "${environment}"
+"${cli_path}" environment:activate -p "${project}" -e "${environment}"
 
+# send media and sql back file
+cat "${backups_dir}/${tar_file}" | ssh -i "${HOME}/.ssh/id_rsa.magento" $("${cli_path}" ssh -p "${project}" -e "${environment}" --pipe) "tar -xf - -C /app pub/media var/backups"
 
-# push to cloud & wait for installation to complete
+# get sql_file name
+sql_file=$(tar -tf tar -tf "${backups_dir}/${tar_file}" | grep "var/backups/.*.sql" | sed "s/.*\///")
 
-# send tar file
-
-# extract files
-
-# replace hostname
-
-# restore db
+# replace hostname and rollback db
+new_base_url=$(curl -sI localhost | sed -n 's/Location: //;s/\.cloud\/.*/.cloud/p')
+perl -i -pe "s!REPLACEMENT_BASE_URL!${new_base_url}!g"
+php bin/magento setup:rollback -n -d "${sql_file}"

ab80f9a77bf9a732d80d9886563db7c1e8b89cdb
Keith Bentrup <keith@keithbentrup.com>
2018-11-29 10:46:14 -0500

    finish initial copy to env script

--- sh-scripts/copy-imgs-to-env.sh
+++ sh-scripts/copy-imgs-to-env.sh
@@ -1,18 +1,20 @@
 printf "\nCopy imgs to an env ...\n"
 
 # prompt user for tar file and project to create new env on
-backtitle="Creating new cloud env from backup ..."
+backtitle="Copy images to an env ..."
 
 img_dirs=($(find "${HOME}/Downloads" -name "imgs-from-*" -type d 2>/dev/null | sort | perl -pe 's!.*/!!' | cat -n))
 if [[ ${#img_dirs[@]} -lt 1 ]]; then
   error No image dirs from the store scraper found in "${HOME}/Downloads"
 fi
 
-selections=$(dialog --clear \
+selection=$(dialog --clear \
   --backtitle "${backtitle}" \
   --title "Your Image Dir(s) From Store Scraper" \
   --menu "Choose a dir to copy:" \
   $menu_height $menu_width $num_visible_choices "${img_dirs[@]}" \
   2>&1 >/dev/tty)
 clear
+img_dir="${img_dirs[$(( (${selection} - 1) * 2 + 1))]}"
 
+$scp_cmd -r "${HOME}/Downloads/${img_dir}/" ${user_and_host}:${home_dir}/pub/media/import/products

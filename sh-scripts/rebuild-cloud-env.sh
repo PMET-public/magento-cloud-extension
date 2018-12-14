3f3d1ead225d6a7aa2c6a9353a77fd82e10cb5a7
Keith Bentrup <keithbentrup@gmail.com>
2018-12-14 11:22:33 -0500

    add rebuild cloud env cmd to force rebuild when needed

---
+++ sh-scripts/rebuild-cloud-env.sh
@@ -0,0 +1,11 @@
+msg Rebuilding env ...
+
+tmp_git_dir="/tmp/delete-me-${project}-${environment}"
+rm -rf "${tmp_git_dir}" # ensure tmp_git_dir doesn't exist from a previously aborted cmd 
+git clone --branch "${environment}" $(${cli_path} project:info -p "${project}" git) "${tmp_git_dir}"
+cd "${tmp_git_dir}"
+date > .force-env-rebuild
+git add -f .force-env-rebuild
+git commit -m "force env rebuild"
+git push
+rm -rf "${tmp_git_dir}" # clean up

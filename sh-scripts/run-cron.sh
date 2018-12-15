8e87ccd758952748f90fd1c80d572e6cfe280828
Keith Bentrup <keithbentrup@gmail.com>
2018-12-15 11:14:14 -0500

    run cron every min for 60 min
    useful for emails during demo & can be canceled just by closing

--- sh-scripts/run-cron.sh
+++ sh-scripts/run-cron.sh
@@ -1,3 +1,7 @@
 printf "\nRunning cron jobs ...\n"
 
-$ssh_cmd "php ${app_dir}/bin/magento cron:run; php ${app_dir}/bin/magento cron:run"
+$ssh_cmd "for i in \$(seq 60 -1 1); do
+    php ${app_dir}/bin/magento cron:run; 
+    echo \$i times remaining. Sleeping 1 min.;
+    sleep 60; 
+  done"

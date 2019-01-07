7dcb04f30160ca39f4f0410019a641d5ad400eea
Keith Bentrup <keithbentrup@gmail.com>
2019-01-07 13:08:10 -0800

    force user interaction for deletion
    --yes wasn't working anyway

--- sh-scripts/delete-env.sh
+++ sh-scripts/delete-env.sh
@@ -1,4 +1,4 @@
 msg Deleting env ... Answer \"Yes\" to both questions below to immediately deactivate AND delete your env.
 
 # --delete-branch appears to be incompatible with the --no-wait flag
-"${cli_path}" environment:delete -p "${project}" -e "${environment}" --yes --delete-branch
+"${cli_path}" environment:delete -p "${project}" -e "${environment}" --delete-branch

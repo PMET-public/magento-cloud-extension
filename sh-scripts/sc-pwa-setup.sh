75959db18d49ebd8091ca7636fd4655da62084d6
Keith Bentrup <keithbentrup@gmail.com>
2019-01-09 15:46:27 -0800

    rewrite of pwa script; also prompt user for update confirmation

--- sh-scripts/sc-pwa-setup.sh
+++ sh-scripts/sc-pwa-setup.sh
@@ -1,49 +1,38 @@
-#!/bin/bash
+printf "\nSetting up PWA Studio ...\n"
 
-# 1st install & run
-# possibly update & run
+NODE_VERSION="v10.15.0"
+GIT_PWA_URL="https://release-dev-rxvv2iq-zddsyhrdimyra.us-4.magentosite.cloud/"
+CLOUD_PWA_URL="https://pwa-n3sumvi-xy4itwbmg2khk.demo.magentosite.cloud/"
 
-if which node > /dev/null; then
-  echo "[*] node is installed, continuing..."
-  if [ -d "$HOME/pwa-studio/node_modules" ]; then
-    echo "[*] pwa-studio previously setup, continuing..."
-    cd $HOME/pwa-studio/
-    LOCAL=$(git rev-parse @)
-    REMOTE=$(git rev-parse @{u})
-    BASE=$(git merge-base @ @{u})
-    if [ $LOCAL = $REMOTE ]; then
-      echo "[*] pwa-studio up-to-date, starting..."
-      npm run watch:venia
-    elif [ $LOCAL = $BASE ]; then
-      echo "[*] pwa-studio update available, updating..."
-      echo "[*] pulling update..."
-      git pull
-      echo "[*] re-installing..."
-      npm install
-      echo "[*] re-building..."
-      npm run build
-      echo "[*] starting..."
-      npm run watch:venia
-    else
-      echo "[*] Diverged, ¯\_(ツ)_/¯"
-      exit 1
-    fi
-  else
-    echo "[*] setting up pwa-studio for the first time..."
-    cd $HOME
-    echo "[*] cloning pwa-studio..."
-    git clone https://github.com/magento-research/pwa-studio.git
-    cd pwa-studio/
-    echo "[*] installing pwa-studio..."
-    npm install
-    cp packages/venia-concept/.env.dist packages/venia-concept/.env
-    echo "[*] configuring storefront url from cloud..."
-    sed -i '' 's,https://release-dev-rxvv2iq-zddsyhrdimyra.us-4.magentosite.cloud/,https://pwa-n3sumvi-xy4itwbmg2khk.demo.magentosite.cloud/,g' packages/venia-concept/.env
-    echo "building the studio..."
-    npm run build
-    echo "starting the studio..."
-    npm run watch:venia
-  fi
-  else
-   echo "[*] visit https://nodejs.org/en/download/ to install to continue"
+if ! which node > /dev/null; then
+  curl -o "/tmp/${NODE_VERSION}.tar.gz" "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-darwin-x64.tar.gz"
+  mkdir -p "${HOME}/${NODE_VERSION}"
+  tar -zxf "/tmp/${NODE_VERSION}.tar.gz" -C "${HOME}/${NODE_VERSION}" --strip 1
+  ln -sf "${HOME}/${NODE_VERSION}/bin/node" /usr/local/bin/node
+  ln -sf "${HOME}/${NODE_VERSION}/bin/npm" /usr/local/bin/npm
+  rm "/tmp/${NODE_VERSION}.tar.gz"
 fi
+
+if [[ ! -d "${HOME}/pwa-studio/node_modules" ]]; then
+  cd "${HOME}"
+  git clone https://github.com/magento-research/pwa-studio.git
+  cd pwa-studio/
+  npm install
+  perl -pe "s!${GIT_PWA_URL}!${CLOUD_PWA_URL}!g" packages/venia-concept/.env.dist > packages/venia-concept/.env
+  npm run build || :
+fi 
+
+cd "${HOME}/pwa-studio/"
+
+if [ "$(git rev-parse @)" -ne $(git rev-parse @{u}) ]; then
+  read -p "There is an update to PWA. It may contain breaking changes. Upgrade? (y/n)" -n 1 -r
+  echo # new line
+  if [[ "${REPLY}" =~ ^[Yy]$ ]]
+  then
+    git pull
+    npm install
+    npm run build || :
+  fi
+fi
+
+npm run watch:venia

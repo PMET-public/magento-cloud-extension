12c0a4f89d031b665643dadca13178d807dd0941
Keith Bentrup <keith@keithbentrup.com>
2019-02-05 23:22:33 -0500

    kill any php process before deleting to avoid orphaned envs

--- sh-scripts/delete-env.sh
+++ sh-scripts/delete-env.sh
@@ -1,4 +1,8 @@
 msg Deleting env ... Answer \"Yes\" to both questions below to immediately deactivate AND delete your env.
 
+# kill any php process up to 10 times in the next 10 min
+# that may still be running and blocking a proper shutdown
+$ssh_cmd "for i in {1..10}; do pkill php; sleep 60; done &" &
+
 # --delete-branch appears to be incompatible with the --no-wait flag
 "${cli_path}" environment:delete -p "${project}" -e "${environment}" --delete-branch

504a1803836034e8da241490f7657eaa243680c9
Keith Bentrup <keith@keithbentrup.com>
2019-02-19 21:13:54 -0500

    update url to proxy readme

--- sh-scripts/configure-proxies.sh
+++ sh-scripts/configure-proxies.sh
@@ -9,5 +9,5 @@ $ssh_cmd -f -D 8889 -L 8888:localhost:8888 "nohup sh -c '/tmp/tinyproxy/tinyprox
 
 echo "printf '${red}\nNotes:\n\n1. Proxy will be open for 2 hrs.\n'"
 echo "printf '2. Composer and ssh only configured for this terminal.\n'"
-echo "printf '3. If you need gitlab web access, configure your browser:\n\thttps://raw.githubusercontent.com/PMET-public/magento-cloud-extension/${ext_ver}/tinyproxy/\n\n${no_color}'"
+echo "printf '3. If you need gitlab web access, configure your browser:\n\thttps://github.com/PMET-public/magento-cloud-extension/tree/${ext_ver}/tinyproxy\n\n${no_color}'"
 echo 'export GIT_SSH_COMMAND='\''ssh -o ProxyCommand="nc -x 127.0.0.1:8100 %h %p"'\'' HTTP_PROXY=http://127.0.0.1:8888; exec bash'

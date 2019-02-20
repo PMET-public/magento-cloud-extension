1d3b70e14149225152d8fa6cf0de82defe423bbd
Keith Bentrup <keith@keithbentrup.com>
2019-02-20 12:40:33 -0500

    fix SOCKS port #; increase proxy timeout; update instructions

--- sh-scripts/configure-proxies.sh
+++ sh-scripts/configure-proxies.sh
@@ -7,11 +7,11 @@ curl -s -o - "https://raw.githubusercontent.com/PMET-public/magento-cloud-extens
 # also start HTTP proxy (tinyproxy) with auto kill
 # proper backgrounding when invoked in subshell: https://stackoverflow.com/questions/50613945/bash-run-command-in-background-inside-subshell
 ssh -n -D 8889 -L 8888:localhost:8888 -i ${identity_file} $(get_ssh_url) \
-  "nohup sh -c 'sleep 7200; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
+  "nohup sh -c 'sleep 14400; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
 
-printf "${red}\nNotes:\n\n1. Proxy opened for 2 hrs.\n" >&2
-printf "2. Composer and ssh are only configured for this terminal.\n" >&2
-printf "3. If you need gitlab web access, instructions to configure your browser can be found here:\n" >&2
+printf "${red}\nNotes:\n\n1. Access granted for 4 hrs or until local/remote processes stopped.\n" >&2
+printf "2. Composer and git cmds are only configured for this terminal.\n" >&2
+printf "3. For gitlab web access (only if you need it), instructions to configure your browser can be found here:\n" >&2
 printf "\thttps://github.com/PMET-public/magento-cloud-extension/tree/${ext_ver}/tinyproxy\n\n${no_color}" >&2
-echo GIT_SSH_COMMAND='ssh -o ProxyCommand="nc -x 127.0.0.1:8100 %h %p"'
+echo GIT_SSH_COMMAND='ssh -o ProxyCommand="nc -x 127.0.0.1:8889 %h %p"'
 echo HTTP_PROXY=http://127.0.0.1:8888

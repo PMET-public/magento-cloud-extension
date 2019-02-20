c5a17ce76f459b9c661c4433023c772b409700b4
Keith Bentrup <keith@keithbentrup.com>
2019-02-20 12:28:43 -0500

    fix backgrounding in subshell invocation

--- sh-scripts/configure-proxies.sh
+++ sh-scripts/configure-proxies.sh
@@ -5,9 +5,13 @@ curl -s -o - "https://raw.githubusercontent.com/PMET-public/magento-cloud-extens
 
 # start SOCKS proxy for git access over ssh via netcat (nc) var below
 # also start HTTP proxy (tinyproxy) with auto kill
-$ssh_cmd -f -D 8889 -L 8888:localhost:8888 "nohup sh -c '/tmp/tinyproxy/tinyproxy -c /tmp/tinyproxy/tinyproxy.conf; sleep 7200; pkill tinyproxy' > /dev/null 2>&1" &
+# proper backgrounding when invoked in subshell: https://stackoverflow.com/questions/50613945/bash-run-command-in-background-inside-subshell
+ssh -n -D 8889 -L 8888:localhost:8888 -i ${identity_file} $(get_ssh_url) \
+  "nohup sh -c 'sleep 7200; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
 
-echo "printf '${red}\nNotes:\n\n1. Proxy will be open for 2 hrs.\n'"
-echo "printf '2. Composer and ssh only configured for this terminal.\n'"
-echo "printf '3. If you need gitlab web access, configure your browser:\n\thttps://github.com/PMET-public/magento-cloud-extension/tree/${ext_ver}/tinyproxy\n\n${no_color}'"
-echo 'export GIT_SSH_COMMAND='\''ssh -o ProxyCommand="nc -x 127.0.0.1:8100 %h %p"'\'' HTTP_PROXY=http://127.0.0.1:8888; exec bash'
+printf "${red}\nNotes:\n\n1. Proxy opened for 2 hrs.\n" >&2
+printf "2. Composer and ssh are only configured for this terminal.\n" >&2
+printf "3. If you need gitlab web access, instructions to configure your browser can be found here:\n" >&2
+printf "\thttps://github.com/PMET-public/magento-cloud-extension/tree/${ext_ver}/tinyproxy\n\n${no_color}" >&2
+echo GIT_SSH_COMMAND='ssh -o ProxyCommand="nc -x 127.0.0.1:8100 %h %p"'
+echo HTTP_PROXY=http://127.0.0.1:8888

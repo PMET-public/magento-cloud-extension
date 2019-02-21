46307c1a08ee74965884aac3421c5be3d526676d
Keith Bentrup <keith@keithbentrup.com>
2019-02-21 16:21:12 -0500

    Update configure-proxies.sh

--- sh-scripts/configure-proxies.sh
+++ sh-scripts/configure-proxies.sh
@@ -13,7 +13,7 @@ curl -s -o - "https://raw.githubusercontent.com/PMET-public/magento-cloud-extens
 # start SOCKS proxy for git access over ssh via netcat (nc) var below
 # also start HTTP proxy (tinyproxy) with auto kill
 # proper backgrounding when invoked in subshell: https://stackoverflow.com/questions/50613945/bash-run-command-in-background-inside-subshell
-ssh -i ${identity_file} $(get_ssh_url ${project} ${environment}) "pkill tinyproxy" || :
+ssh -i ${identity_file} $(get_ssh_url ${project} ${environment}) "pkill tinyproxy || :"
 ssh -n -D 8889 -L 8888:localhost:8888 -i ${identity_file} $(get_ssh_url ${project} ${environment}) \
   "nohup sh -c 'sleep 14400; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
 

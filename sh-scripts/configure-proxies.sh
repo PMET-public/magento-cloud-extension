ab1425d4c2391bde3a1c2058cba4fc9dabee5142
Keith Bentrup <keith@keithbentrup.com>
2019-02-21 15:41:49 -0500

    kill any existing proxy to restart timer

--- sh-scripts/configure-proxies.sh
+++ sh-scripts/configure-proxies.sh
@@ -14,7 +14,7 @@ curl -s -o - "https://raw.githubusercontent.com/PMET-public/magento-cloud-extens
 # also start HTTP proxy (tinyproxy) with auto kill
 # proper backgrounding when invoked in subshell: https://stackoverflow.com/questions/50613945/bash-run-command-in-background-inside-subshell
 ssh -n -D 8889 -L 8888:localhost:8888 -i ${identity_file} $(get_ssh_url ${project} ${environment}) \
-  "nohup sh -c 'sleep 14400; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
+  "pkill tinyproxy; nohup sh -c 'sleep 14400; pkill tinyproxy' > /dev/null 2>&1 & /tmp/tinyproxy/tinyproxy -d -c /tmp/tinyproxy/tinyproxy.conf" > /dev/null &
 
 printf "${red}\nNotes:\n\n1. Access granted for 4 hrs or until local/remote processes stopped.\n" >&2
 printf "2. Composer and git cmds are only configured for this terminal.\n" >&2

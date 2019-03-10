bf9207536002b368c06fb6d24374a90f7f8139df
Keith Bentrup <keithbentrup@gmail.com>
2019-03-10 01:46:29 -0400

    modify backup list

--- sh-scripts/backup-env.sh
+++ sh-scripts/backup-env.sh
@@ -20,7 +20,21 @@ $ssh_cmd "mysqldump ${db_opts} --single-transaction --no-autocommit --quick > ${
   gzip ${sql_file}
 
   # add sql file and other files needed to recreate project/environment
-  tar --ignore-failed-read -C / -cf ${remote_tar_file} ${sql_file}.gz ${app_dir}/auth.json ${app_dir}/.gitignore ${app_dir}/composer.json ${app_dir}/composer.lock ${additional_files} ${app_dir}/pub/media/gene-cms ${app_dir}/pub/media/wysiwyg ${app_dir}/pub/media/ThemeCustomizer 2> /dev/null
+  tar --ignore-failed-read -C / -cf ${remote_tar_file} ${sql_file}.gz \
+    --exclude="${app_dir}/pub/media/catalog/product/cache" \
+    ${app_dir}/auth.json \
+    ${app_dir}/.gitignore \
+    ${app_dir}/composer.json \
+    ${app_dir}/composer.lock \
+    ${app_dir}/app/etc/env.php \
+    ${app_dir}/app/etc/config.php \
+    ${app_dir}/m2-hotfixes \
+    ${additional_files} \
+    ${app_dir}/pub/media/gene-cms \
+    ${app_dir}/pub/media/wysiwyg \
+    ${app_dir}/pub/media/ThemeCustomizer \
+    ${app_dir}/pub/media/catalog/product \
+    2> /dev/null
   rm ${sql_file}.gz
 
   # find full paths of imported images and create tar file

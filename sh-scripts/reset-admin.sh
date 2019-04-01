5ca6278e25b2966c6bd059c6f149c01bfef10297
Keith Bentrup <keithbentrup@gmail.com>
2019-04-01 07:21:31 -0400

    use msg function instead of printf to print status

--- sh-scripts/reset-admin.sh
+++ sh-scripts/reset-admin.sh
@@ -1,4 +1,4 @@
-printf "\nCreating/resetting admin user ...\n"
+msg Creating/resetting admin user ...
 
 $ssh_cmd "php ${app_dir}/bin/magento admin:user:unlock ${user};
   php ${app_dir}/bin/magento admin:user:create --admin-user=${user} --admin-password=${password} --admin-email=${email} --admin-firstname=Admin --admin-lastname=Username"

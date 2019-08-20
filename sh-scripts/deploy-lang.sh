4e6a4b2ad9ac40c9683046234f1b2d173717403e
Keith Bentrup <keith@keithbentrup.com>
2019-08-20 11:16:13 -0400

    add line to replace invalid lang codes

--- sh-scripts/deploy-lang.sh
+++ sh-scripts/deploy-lang.sh
@@ -1,7 +1,11 @@
 msg Deploying all languages ...
 
 $ssh_cmd "
+  # find languages
   LANGS=\$(find vendor -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
+    # fix invalid lang codes
+    perl -pe 's/zh_CN/zh_Hans_CN/;s/sr_SP/sr_Cyrl_RS/;s/zh_TW/zh_Hant_TW/' | \
+    # remove duplicates
     sort -u)
   php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard \${LANGS}
 "

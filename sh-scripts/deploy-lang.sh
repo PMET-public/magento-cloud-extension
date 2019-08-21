c410be75f731713ad59f4e32a3e28d5d94bfe7a4
Keith Bentrup <keith@keithbentrup.com>
2019-08-20 22:30:08 -0400

    prompt user & deploy just one lang at a time

--- sh-scripts/deploy-lang.sh
+++ sh-scripts/deploy-lang.sh
@@ -1,12 +1,14 @@
-msg Deploying all languages ...
+msg Finding available languages ...
 
 $ssh_cmd "
   # find languages
-  LANGS=\$(find vendor -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
+  find vendor -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
     # fix invalid lang codes
     perl -pe 's/zh_CN/zh_Hans_CN/;s/sr_SP/sr_Cyrl_RS/;s/zh_TW/zh_Hant_TW/' | \
     # remove duplicates
-    sort -u)
-  php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard \${LANGS}
+    sort -u
+  read -p 'Choose language code to deploy: ' lang_code 
+  php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard \${lang_code}
+  echo Cleaning layout cache ...
   php ./bin/magento cache:clean layout
 "

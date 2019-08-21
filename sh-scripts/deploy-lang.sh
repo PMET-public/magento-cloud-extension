86208123e096d8b4b248386c08865447cd77efd1
Keith Bentrup <keith@keithbentrup.com>
2019-08-21 00:46:27 -0400

    attempt alt method to read from stdin

--- sh-scripts/deploy-lang.sh
+++ sh-scripts/deploy-lang.sh
@@ -1,7 +1,5 @@
 msg Finding available languages ...
 
-ssh_cmd=$(get_interactive_ssh_cmd ${project} ${environment})
-
 $ssh_cmd "
   # find languages
   find vendor -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
@@ -9,13 +7,17 @@ $ssh_cmd "
     perl -pe 's/zh_CN/zh_Hans_CN/;s/sr_SP/sr_Cyrl_RS/;s/zh_TW/zh_Hant_TW/' | \
     # remove duplicates
     sort -u
-  echo Which language code to deploy?
-  read lang_code
-  if [[ ! -z \"\${lang_code}\" ]]; then
-    php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard \${lang_code}
+  "
+
+echo Which language code to deploy?
+read lang_code
+
+if [[ ! -z \"\${lang_code}\" ]]; then
+  $ssh_cmd "
+    php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard ${lang_code}
     echo Cleaning layout cache ...
     php ./bin/magento cache:clean layout
-  else
-    echo No code entered.
-  fi
-"
+  "
+else
+  echo No code entered.
+fi

18eb28e51b282d373b62ed300690a669ba685ebd
Keith Bentrup <keith@keithbentrup.com>
2019-11-13 09:31:01 -0500

    link update

--- app/scripts/content/document-start.js
+++ app/scripts/content/document-start.js
@@ -1,6 +1,13 @@
 'use strict'
 MCExt.loadSavedCSS()
 if (MCExt.isCurrentTabCloudProjects()) {
+  function addPassToEnvLink(e) { 
+    if (e.target && e.target.href && /demo.magentosite.cloud/.test(e.target.href)) {
+      e.target.href = e.target.href.replace(/(https?:\/\/)(.*-)(.*)(\.demo\.magentosite\.cloud)/, '$1admin:$3@$2$3$4')
+    }
+  }
+  window.addEventListener('click', addPassToEnvLink)
+  window.addEventListener('contextmenu', addPassToEnvLink)
   MCExt.loadCSS(MCExt.cloudUiCss)
 } else if (MCExt.isCurrentTabCloudEnv()) {
   console.log('cloud env')

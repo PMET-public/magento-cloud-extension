2372c62ca0e7e71b689ddbad191bf193adf6bb31
Keith Bentrup <keith@keithbentrup.com>
2019-11-13 10:44:09 -0500

    update paths and how to find new version

--- sh-scripts/update-extension.sh
+++ sh-scripts/update-extension.sh
@@ -1,9 +1,11 @@
 msg Downloading and extracting extension ...
 
-rm ~/Downloads/mcm-chrome-ext/mcm-chrome-ext.zip || :
+rm ~/Downloads/mcm-chrome-ext.zip || :
 
-curl -L -o ~/Downloads/mcm-chrome-ext/mcm-chrome-ext.zip --create-dirs https://github.com/PMET-public/magento-cloud-extension/releases/download/${ext_ver}/mcm-chrome-ext-${ext_ver}.zip
+master_ver=$(curl -ss https://raw.githubusercontent.com/PMET-public/magento-cloud-extension/master/app/manifest.json | perl -ne 's/^\s*"version"\s*:\s*"(.*)".*/\1/ and print')
 
-find ~/Downloads/mcm-chrome-ext -type f ! -name 'manifest.json' -a ! -name 'mcm-chrome-ext.zip' -delete
+curl -L -o ~/Downloads/mcm-chrome-ext.zip --create-dirs https://github.com/PMET-public/magento-cloud-extension/releases/download/${master_ver}/mcm-chrome-ext.zip
 
-unzip -o -d ~/Downloads/mcm-chrome-ext ~/Downloads/mcm-chrome-ext/mcm-chrome-ext.zip
+find ~/Downloads/mcm-chrome-ext -type f ! -name 'manifest.json' -delete
+
+unzip -o -d ~/Downloads/mcm-chrome-ext ~/Downloads/mcm-chrome-ext.zip

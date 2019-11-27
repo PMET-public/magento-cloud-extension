210105035ad4c19bb102f2725b1f88a08359ea3d
Keith Bentrup <keith@keithbentrup.com>
2019-11-27 07:08:28 -0500

    remove old admin script

--- sh-scripts/reset-admin.sh
+++
@@ -1,4 +0,0 @@
-msg Creating/resetting admin user ...
-
-$ssh_cmd "php ${app_dir}/bin/magento admin:user:unlock ${user};
-  php ${app_dir}/bin/magento admin:user:create --admin-user=${user} --admin-password=${password} --admin-email=${email} --admin-firstname=Admin --admin-lastname=Username"

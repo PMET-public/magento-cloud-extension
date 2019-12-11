35c88084db56de3b5b38be9c53f8ee09d20d0dac
Keith Bentrup <keith@keithbentrup.com>
2019-12-11 01:17:07 -0500

    prompt user for admin username

--- sh-scripts/admin-unlock.sh
+++ sh-scripts/admin-unlock.sh
@@ -1,3 +1,10 @@
-msg Creating/resetting admin user ...
+msg Unlocking admin user ...
+
+read -p "Enter username of admin account or leave blank for 'admin':
+" -n 1 -r < /dev/tty
+
+if [[ -z "${REPLY}" ]]; then
+  user=admin
+fi
 
 $ssh_cmd "php ${app_dir}/bin/magento admin:user:unlock ${user};"

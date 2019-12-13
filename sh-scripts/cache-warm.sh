d25933ff670214d51c9b699594b58763a2d3e35a
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 18:09:26 -0500

    reduce output; skip most static files

--- sh-scripts/cache-warm.sh
+++ sh-scripts/cache-warm.sh
@@ -2,10 +2,11 @@ msg Warming cache ...
 
 wget_url=$(echo "${tab_url}" | perl -pe "s!^(https?://[^/]+).*!\1!")
 wget_domain=$(echo "${wget_url}" | perl -pe "s!https?://!!")
-
 tmp_file=/tmp/$(date '+delete-me-%Y-%m-%d-%H-%M-%S')]
 
 # recursively get admin and store front
-$ssh_cmd "wget -r -l 1 -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}/admin
-  wget -r -l 1 -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}
-  rm ${tmp_file}"
+$ssh_cmd "
+  wget -nv -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}/admin
+  wget -nv -r -X static,media -l 1 -O ${tmp_file} -H --domains=${wget_domain} ${wget_url}
+  rm ${tmp_file}
+"

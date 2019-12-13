9d787834862712776ab6be9a5ed78010e24f9e98
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 10:59:25 -0500

    fix vm cron

--- sh-scripts/remote/updates-to-run-directly-on-vm.sh
+++ sh-scripts/remote/updates-to-run-directly-on-vm.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-
 #!/bin/bash
 
 if [[ -z "$debug" || $debug -eq 1 ]]; then
@@ -7,7 +5,5 @@ if [[ -z "$debug" || $debug -eq 1 ]]; then
   set -e
 fi
 
-# make it easy to call via bash history or while writing/debugging
-[[ "$0" =~ "most-recent" ]] || rm /tmp/most-recent-vm-updates.sh || ln -s $0 /tmp/most-recent-vm-updates.sh
-
-touch /tmp/delme-$(date +%s)
+# keep cron success status msg 
+crontab -l | perl -pe 's/Ran jobs by schedule/DONT REMOVE CRON STATUS MSG/' | crontab -
--- sh-scripts/update-vm.sh
+++ sh-scripts/update-vm.sh
@@ -1,10 +1,4 @@
-msg Diagnosing, attempting to repair, and reporting ...
+msg Downloading and running update script ...
 
-# fetch the mcm version
 # and turn off debugging (by default, debugging is on)
-$ssh_cmd "curl -s $ext_raw_git_url/sh_scripts/remote/updates-to-run-directly-on-vm.sh | env debug=${debug:=0} bash" |
-  tee /dev/tty |
-  # copy report to clipboard and strip color characters
-  perl -pe 's/\e\[\d+(?>(;\d+)*)m//g;' | pbcopy
-
-
+$ssh_cmd "curl -s $ext_raw_git_url/sh-scripts/remote/updates-to-run-directly-on-vm.sh | env debug=${debug:=0} bash"

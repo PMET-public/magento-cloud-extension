434546d15b967f631c3dc0cd8caf572d96158f75
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 07:32:20 -0500

    send results to stdout and clipboard

--- sh-scripts/diagnose-repair-report.sh
+++ sh-scripts/diagnose-repair-report.sh
@@ -1,3 +1,9 @@
 msg Diagnosing, attempting to repair, and reporting ...
 
-$ssh_cmd "curl -s https://raw.githubusercontent.com/PMET-public/magento-cloud-manager/diagnose-repair-report/cli/bin/env-scripts/diagnose-repair-report.sh | bash"
+# fetch the mcm version
+# and turn off debugging (by default, debugging is on)
+$ssh_cmd "curl -s https://raw.githubusercontent.com/PMET-public/magento-cloud-manager/diagnose-repair-report/cli/bin/env-scripts/diagnose-repair-report.sh | env debug=${debug:=0} bash" 2>/dev/null |
+  tee /dev/tty |
+  # copy report to clipboard and strip color characters
+  perl -pe 's/\e\[\d+(?>(;\d+)*)m//g;' | pbcopy
+

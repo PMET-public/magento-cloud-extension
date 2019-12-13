4abc2aa4c2ead3be1865e4b53bc6472b5cdfe620
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 11:00:23 -0500

    add clarifying comment; improve readability

--- sh-scripts/diagnose-repair-report.sh
+++ sh-scripts/diagnose-repair-report.sh
@@ -1,8 +1,9 @@
 msg Diagnosing, attempting to repair, and reporting ...
 
-# fetch the mcm version
+# fetch the mcm version of the diagnose, repair, report script
 # and turn off debugging (by default, debugging is on)
-$ssh_cmd "curl -s https://raw.githubusercontent.com/PMET-public/magento-cloud-manager/diagnose-repair-report/cli/bin/env-scripts/diagnose-repair-report.sh | env debug=${debug:=0} bash" 2>/dev/null |
+$ssh_cmd "curl -s https://raw.githubusercontent.com/PMET-public/magento-cloud-manager/diagnose-repair-report/cli/bin/env-scripts/diagnose-repair-report.sh | 
+  env debug=${debug:=0} bash" |
   tee /dev/tty |
   # copy report to clipboard and strip color characters
   perl -pe 's/\e\[\d+(?>(;\d+)*)m//g;' | pbcopy

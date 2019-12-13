e2348e7266688aaff5c41de8ebf3f97756014d80
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 15:28:00 -0500

    change 'msg' to 'warning' on screen capture

--- sh-scripts/screen-capture.sh
+++ sh-scripts/screen-capture.sh
@@ -2,4 +2,4 @@ msg Select a window to capture to the clipboard ...
 
 screencapture -i -c -o -W
 
-msg Copied to clipboard! Now paste \( command ⌘ + v \) into slack or any other application.
+warning Copied to clipboard! Now paste \( command ⌘ + v \) into slack or any other application.

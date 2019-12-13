fa7c0fce3a395ff26528f69c11589bae1aed0d8e
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 18:16:02 -0500

    add how to stop msg

--- sh-scripts/watch-logs.sh
+++ sh-scripts/watch-logs.sh
@@ -1,3 +1,4 @@
+warning Press control-c to stop or simply close terminal.
 #  $ssh_cmd "stdbuf -oL tail -f /var/log/php.access.log ${app_dir}/var/log/system.log ${app_dir}/var/log/exception.log | \
 #    stdbuf -oL perl -pe 's!${app_dir}/var/log/system.log!${green}\$&${no_color}!; s!${app_dir}/var/log/exception.log!${red}\$&${no_color}!;'"
 

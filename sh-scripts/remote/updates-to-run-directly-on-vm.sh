b215ea7094cf5ce452cf85cd5a1423803062fd9a
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 13:00:14 -0500

    add some lib vars & func; fix repeated run error

--- sh-scripts/remote/updates-to-run-directly-on-vm.sh
+++ sh-scripts/remote/updates-to-run-directly-on-vm.sh
@@ -5,5 +5,18 @@ if [[ -z "$debug" || $debug -eq 1 ]]; then
   set -e
 fi
 
-# keep cron success status msg 
-crontab -l | perl -pe 's/Ran jobs by schedule/DONT REMOVE CRON STATUS MSG/' | crontab -
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[1;33m'
+no_color='\033[0m'
+cur_unix_ts=$(date +%s)
+
+report () {
+  printf "${@}" | tee -a /tmp/$cur_unix_ts-report.log
+}
+
+# keep cron success status msg
+crontab -l |
+  tee /tmp/$cur_unix_ts-crontab |
+  perl -pe 's/Ran jobs by schedule/DONT REMOVE CRON STATUS MSG/' |
+  crontab -

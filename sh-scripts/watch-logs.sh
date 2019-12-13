465ecae080dc435500a4187e52e307b0e4e20a7a
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 18:10:21 -0500

    add logic to determine correct paths

--- sh-scripts/watch-logs.sh
+++ sh-scripts/watch-logs.sh
@@ -1,6 +1,8 @@
-printf "\nWatching logs ...\n"
-
 #  $ssh_cmd "stdbuf -oL tail -f /var/log/php.access.log ${app_dir}/var/log/system.log ${app_dir}/var/log/exception.log | \
 #    stdbuf -oL perl -pe 's!${app_dir}/var/log/system.log!${green}\$&${no_color}!; s!${app_dir}/var/log/exception.log!${red}\$&${no_color}!;'"
 
-$ssh_cmd "stdbuf -oL tail -f /var/log/php.access.log ${app_dir}/var/log/system.log ${app_dir}/var/log/exception.log"
+is_cloud &&
+  access_log=/var/log/access.log ||
+  access_log=/var/log/nginx/access.log
+
+$ssh_cmd "stdbuf -oL tail -n 3 -f $access_log ${app_dir}/var/log/system.log ${app_dir}/var/log/exception.log"

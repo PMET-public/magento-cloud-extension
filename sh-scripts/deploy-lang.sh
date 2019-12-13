88579ce841e431c2144c9eff97a232ba045afe18
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 18:08:54 -0500

    update msging; reduce search scope for lang files

--- sh-scripts/deploy-lang.sh
+++ sh-scripts/deploy-lang.sh
@@ -1,8 +1,9 @@
-msg Finding available languages ...
+msg Finding available languages. May take a minute ...
 
 $ssh_cmd "
-  # find languages
-  find vendor -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
+  cd $app_dir/vendor
+  # find languages in known directories
+  find magento community-engineering splendidinternet -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
     # fix invalid lang codes
     perl -pe 's/zh_CN/zh_Hans_CN/;s/sr_SP/sr_Cyrl_RS/;s/zh_TW/zh_Hant_TW/' | \
     # remove duplicates
@@ -15,9 +16,10 @@ read lang_code </dev/tty
 
 if [[ ! -z "${lang_code}" ]]; then
   $ssh_cmd "
-    php ./bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard ${lang_code}
+    cd $app_dir
+    php bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard ${lang_code}
     echo Cleaning layout cache ...
-    php ./bin/magento cache:clean layout
+    php bin/magento cache:clean layout
   "
 else
   echo No code entered.

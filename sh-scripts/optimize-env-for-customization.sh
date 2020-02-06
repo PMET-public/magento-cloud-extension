c7afd8388636b085a78ff98da8556660258a685f
Keith Bentrup <keith@keithbentrup.com>
2020-02-06 00:16:04 -0500

    account for app_dir

--- sh-scripts/optimize-env-for-customization.sh
+++ sh-scripts/optimize-env-for-customization.sh
@@ -1,7 +1,7 @@
 $cmd_prefix "
-  php bin/magento config:set dev/js/enable_js_bundling 0
-  php bin/magento config:set dev/js/minify_files 0
-  php bin/magento config:set dev/js/merge_files 0
-  php bin/magento config:set dev/css/merge_css_files 0
-  php bin/magento config:set dev/css/minify_files 0
+  php ${app_dir}/bin/magento config:set dev/js/enable_js_bundling 0
+  php ${app_dir}/bin/magento config:set dev/js/minify_files 0
+  php ${app_dir}/bin/magento config:set dev/js/merge_files 0
+  php ${app_dir}/bin/magento config:set dev/css/merge_css_files 0
+  php ${app_dir}/bin/magento config:set dev/css/minify_files 0
 "

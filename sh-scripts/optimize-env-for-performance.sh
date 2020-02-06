d6775f93beef34ff8a951f35a615f2283a5e9be6
Keith Bentrup <keith@keithbentrup.com>
2020-02-06 00:28:27 -0500

    disable merging when bundling and use static signing according to devdocs

--- sh-scripts/optimize-env-for-performance.sh
+++ sh-scripts/optimize-env-for-performance.sh
@@ -1,7 +1,10 @@
+# https://devdocs.magento.com/guides/v2.3/frontend-dev-guide/themes/js-bundling.html
+
 $cmd_prefix "
+  php ${app_dir}/bin/magento config:set dev/static/sign 1
   php ${app_dir}/bin/magento config:set dev/js/enable_js_bundling 1
   php ${app_dir}/bin/magento config:set dev/js/minify_files 1
-  php ${app_dir}/bin/magento config:set dev/js/merge_files 1
+  php ${app_dir}/bin/magento config:set dev/js/merge_files 0
   php ${app_dir}/bin/magento config:set dev/css/merge_css_files 1
   php ${app_dir}/bin/magento config:set dev/css/minify_files 1
 "

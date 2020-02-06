2e23d3debdf69cfe738145c5ee63fa0cf84aa825
Keith Bentrup <keith@keithbentrup.com>
2020-02-05 23:50:57 -0500

    add options for bundling/unbundling css & js

--- sh-scripts/optimize-env-for-performance.sh
+++ sh-scripts/optimize-env-for-performance.sh
@@ -2,4 +2,6 @@ $cmd_prefix "
   php bin/magento config:set dev/js/enable_js_bundling 1
   php bin/magento config:set dev/js/minify_files 1
   php bin/magento config:set dev/js/merge_files 1
+  php bin/magento config:set dev/css/merge_css_files 1
+  php bin/magento config:set dev/css/minify_files 1
 "

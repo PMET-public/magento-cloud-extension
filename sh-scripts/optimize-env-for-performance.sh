1b4646d8158458fbb47323ac9719eab5bdb4ce45
Keith Bentrup <keith@keithbentrup.com>
2020-02-06 00:54:29 -0500

    disable js/css minifying for now

--- sh-scripts/optimize-env-for-performance.sh
+++ sh-scripts/optimize-env-for-performance.sh
@@ -4,8 +4,8 @@
 $cmd_prefix "
   php ${app_dir}/bin/magento config:set dev/static/sign 1
   php ${app_dir}/bin/magento config:set dev/js/enable_js_bundling 1
-  php ${app_dir}/bin/magento config:set dev/js/minify_files 1
+  php ${app_dir}/bin/magento config:set dev/js/minify_files 0
   php ${app_dir}/bin/magento config:set dev/js/merge_files 0
   php ${app_dir}/bin/magento config:set dev/css/merge_css_files 1
-  php ${app_dir}/bin/magento config:set dev/css/minify_files 1
+  php ${app_dir}/bin/magento config:set dev/css/minify_files 0
 "

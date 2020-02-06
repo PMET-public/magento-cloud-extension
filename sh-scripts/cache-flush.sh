b197472e54cbf3ee3eb7b87f1f1c6b29b5f44f5c
Keith Bentrup <keith@keithbentrup.com>
2020-02-06 00:15:30 -0500

    more consistent formatting

--- sh-scripts/cache-flush.sh
+++ sh-scripts/cache-flush.sh
@@ -1,3 +1,6 @@
 msg Flushing cache ...
 
-$cmd_prefix "php ${app_dir}/bin/magento cache:flush; rm -rf ${app_dir}/var/cache/* ${app_dir}/var/page_cache/*"
+$cmd_prefix "
+  php ${app_dir}/bin/magento cache:flush
+  rm -rf ${app_dir}/var/cache/* ${app_dir}/var/page_cache/*
+"

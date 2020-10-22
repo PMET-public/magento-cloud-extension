867bc01bb8059778e20ce5df7c88a15dd71d6cc6
Keith Bentrup <keith@keithbentrup.com>
2020-10-22 14:42:56 -0400

    mc login when API token is set returns 1 so prevent exit

--- sh-scripts/install-cli-and-login.sh
+++ sh-scripts/install-cli-and-login.sh
@@ -4,4 +4,4 @@
 printf "\n\033[0;32mInstalling magento-cloud CLI ...\033[0m\n\n"
 
 curl -sS https://accounts.magento.cloud/cli/installer | php
-~/.magento-cloud/bin/magento-cloud login
+~/.magento-cloud/bin/magento-cloud login || :

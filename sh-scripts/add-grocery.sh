a18088470b1642fe7092d7fe8e1c8c7544edacca
Keith Bentrup <keith@keithbentrup.com>
2020-11-03 13:42:08 -0500

    allow composer to use more memory

--- sh-scripts/add-grocery.sh
+++ sh-scripts/add-grocery.sh
@@ -8,7 +8,7 @@ git clone --branch "$environment" "$project@git.demo.magento.cloud:$project.git"
 cd "$tmp_git_dir" || exit
 git config user.email "chrome-extension@email.com"
 git config user.name "chrome-extension"
-composer update magentoese/module-data-install magento/services-connector --ignore-platform-reqs
+COMPOSER_MEMORY_LIMIT=-1 composer update magentoese/module-data-install magento/services-connector --ignore-platform-reqs
 composer config repositories.grocery git git@github.com:PMET-public/module-storystore-grocery.git
 composer require story-store/grocery:dev-demo --ignore-platform-reqs
 git add composer.*

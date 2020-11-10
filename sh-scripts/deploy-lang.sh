9312a855b6cd527d6fcc80d0a52607ea41522e28
Keith Bentrup <keith@keithbentrup.com>
2020-11-10 09:35:35 -0500

    prompt user for now required param: store_code

--- sh-scripts/deploy-lang.sh
+++ sh-scripts/deploy-lang.sh
@@ -3,24 +3,33 @@ msg "Finding available languages. May take a minute ..."
 $cmd_prefix "
   cd $app_dir/vendor
   # find languages in known directories
-  find magento community-engineering splendidinternet -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; | \
+  find magento community-engineering splendidinternet -name language.xml -exec perl -ne '/<code>(.*)<\/code>/ and print \"\$1\n\"' {} \; 2> /dev/null | \
     # fix invalid lang codes
     perl -pe 's/zh_CN/zh_Hans_CN/;s/sr_SP/sr_Cyrl_RS/;s/zh_TW/zh_Hant_TW/' | \
     # remove duplicates
     sort -u
   "
 
-echo
-echo ^^ Which language code to deploy?
+echo "
+^^ Which language code to deploy?"
 read lang_code < "$read_input_src"
 
-if [[ ! -z "${lang_code}" ]]; then
+
+$cmd_prefix "php bin/magento store:list"
+
+echo "
+^^ Which store code to deploy? (Must be 'Code' not 'ID' - e.g. default)"
+read store_code < "$read_input_src"
+
+if [[ "$lang_code" =~ ^[a-zA-Z] && "$store_code" =~ ^[a-zA-Z] ]]; then
   $cmd_prefix "
     cd $app_dir
-    php bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard ${lang_code}
+    php bin/magento setup:static-content:deploy --ansi --no-interaction -f --jobs $(nproc) -s standard $lang_code
+    php bin/magento config:set general/locale/code $lang_code --scope=store --scope-code=$store_code
     echo Cleaning layout cache ...
     php bin/magento cache:clean layout
   "
 else
-  echo No code entered.
+  echo "$lang_code sc $store_code"
+  error "Invalid selections."
 fi

9294e0605dca4fccf1144977621c54b0fa71eeab
Keith Bentrup <keith@keithbentrup.com>
2020-12-21 14:04:54 -0500

    fix typo

--- sh-scripts/dl-mdm-lite.sh
+++ sh-scripts/dl-mdm-lite.sh
@@ -8,7 +8,7 @@ cd "$HOME/Downloads"
 curl -sL -o mdm.zip https://github.com/PMET-public/mdm/releases/download/1.0.12/MDM-lite.app.zip
 unzip mdm
 rm mdm.zip
-chmod +x MDM-lite-2.app/Contents/MacOS/MDM-lite
+chmod +x MDM-lite.app/Contents/MacOS/MDM-lite
 # https://apple.stackexchange.com/questions/202169/how-can-i-open-an-app-from-an-unidentified-developer-without-using-the-gui
 xattr -dr com.apple.quarantine "MDM-lite.app"
 open MDM-lite.app

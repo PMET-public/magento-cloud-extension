d430db16288d90c44aafcf4984a054dd544b94e1
Keith Bentrup <keith@keithbentrup.com>
2021-01-19 08:24:13 -0500

    when extracting, overwrite files

--- sh-scripts/dl-mdm-lite.sh
+++ sh-scripts/dl-mdm-lite.sh
@@ -1,12 +1,12 @@
 # shellcheck shell=bash
 : || source lib.sh # trick shellcheck into finding certain referenced vars
 
-msg "Downloading MDM-lite ..."
+echo "Downloading MDM-lite ..."
 
-rm -rf MDM-lite.app
 cd "$HOME/Downloads"
+rm -rf MDM-lite.app || :
 curl -sL -o mdm.zip https://github.com/PMET-public/mdm/releases/download/1.0.12/MDM-lite.app.zip
-unzip mdm
+unzip -uo mdm
 rm mdm.zip
 chmod +x MDM-lite.app/Contents/MacOS/MDM-lite
 # https://apple.stackexchange.com/questions/202169/how-can-i-open-an-app-from-an-unidentified-developer-without-using-the-gui

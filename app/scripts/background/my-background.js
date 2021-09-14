dc21de786a3777a06e1b9691f5cd9f7ba89c137d
Keith Bentrup <keith@keithbentrup.com>
2021-09-14 12:51:56 -0400

    add conditional to fix chrome error report

--- app/scripts/background/my-background.js
+++ app/scripts/background/my-background.js
@@ -1,4 +1,7 @@
 'use strict'
 
 handleExtensionIcon()
-chrome.browserAction.setBadgeBackgroundColor({color: '#ff6d00'})
+// test if function exists b/c chrome reports error in chrome://extensions view under some circumstances
+if (chrome.browserAction.setBadgeBackgroundColor) {
+  chrome.browserAction.setBadgeBackgroundColor({color: '#ff6d00'})
+}

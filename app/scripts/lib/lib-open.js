6adb559bdd543527f6d4a273682d68feb1f9bf52
Keith Bentrup <keith@keithbentrup.com>
2021-09-14 12:53:30 -0400

    run eslint rules against files: 1 out of normal path & 1 with intentional compilation issue

--- app/scripts/lib/lib-open.js
+++ app/scripts/lib/lib-open.js
@@ -1,44 +1,44 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-function handleExtensionIcon() {
-  chrome.storage.local.get(['isCssInjectorOn'], function (result) {
-    const isOn = result['isCssInjectorOn']
-    chrome.browserAction.setBadgeText({text: isOn ? 'on' : ''})
-    chrome.browserAction.setIcon({
-      path: {
-        '16': '../images/cloud-' + (isOn ? '' : 'disabled-') + '16.png',
-        '24': '../images/cloud-' + (isOn ? '' : 'disabled-') + '24.png',
-        '32': '../images/cloud-' + (isOn ? '' : 'disabled-') + '32.png',
-        '48': '../images/cloud-' + (isOn ? '' : 'disabled-') + '48.png'
-      }
+  function handleExtensionIcon() {
+    chrome.storage.local.get(['isCssInjectorOn'], function (result) {
+      const isOn = result['isCssInjectorOn']
+      chrome.browserAction.setBadgeText({text: isOn ? 'on' : ''})
+      chrome.browserAction.setIcon({
+        path: {
+          '16': '../images/cloud-' + (isOn ? '' : 'disabled-') + '16.png',
+          '24': '../images/cloud-' + (isOn ? '' : 'disabled-') + '24.png',
+          '32': '../images/cloud-' + (isOn ? '' : 'disabled-') + '32.png',
+          '48': '../images/cloud-' + (isOn ? '' : 'disabled-') + '48.png'
+        }
+      })
+      checkExtUpdateAvailable().then(available => {
+        if (available) {
+          chrome.browserAction.setBadgeText({text: 'up ⇪'})
+        }
+      })
     })
-    checkExtUpdateAvailable().then(available => {
-      if (available) {
-        chrome.browserAction.setBadgeText({text: 'up ⇪'})
-      }
-    })
-  })
-}
-
-async function checkExtUpdateAvailable() {
-  const response = await fetch('https://raw.githubusercontent.com/PMET-public/magento-cloud-extension/master/app/manifest.json')
-  const json = await response.json()
-  const remoteManifestParts = json.version.split('.')
-  var remoteIsNewer = false
-  for (let i = 0; i < remoteManifestParts.length; i++) {
-    if (parseInt(remoteManifestParts[i], 10) > parseInt(curManifestVersion.split('.')[i], 10)) {
-      remoteIsNewer = true;
-      break;
-    }
   }
-  // return remoteIsNewer || true
-  return remoteIsNewer
-}
 
-// replace method removes any password
-const tabUrl = tabs[0].url.replace(/:\/\/[^/]*@/,'://')
-const tabBaseUrl = tabUrl.substring(0,tabUrl.indexOf('/', 8))
-const appliedDomain = tabBaseUrl.replace(/.*\/\//,'')
-const curManifestVersion = chrome.runtime.getManifest().version
-const isCloud = /\.magento(site)?\.cloud/.test(tabBaseUrl)
-const isVM = !isCloud
+  async function checkExtUpdateAvailable() {
+    const response = await fetch('https://raw.githubusercontent.com/PMET-public/magento-cloud-extension/master/app/manifest.json'),
+      json = await response.json(),
+      remoteManifestParts = json.version.split('.')
+    let remoteIsNewer = false
+    for (let i = 0; i < remoteManifestParts.length; i++) {
+      if (parseInt(remoteManifestParts[i], 10) > parseInt(curManifestVersion.split('.')[i], 10)) {
+        remoteIsNewer = true
+        break
+      }
+    }
+    // return remoteIsNewer || true
+    return remoteIsNewer
+  }
+
+  // replace method removes any password
+  const tabUrl = tabs[0].url.replace(/:\/\/[^/]*@/,'://'),
+    tabBaseUrl = tabUrl.substring(0,tabUrl.indexOf('/', 8)),
+    appliedDomain = tabBaseUrl.replace(/.*\/\//,''),
+    curManifestVersion = chrome.runtime.getManifest().version,
+    isCloud = /\.magento(site)?\.cloud/.test(tabBaseUrl),
+    isVM = !isCloud

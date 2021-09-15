68446db918d12f163c39c1955eb039c1a750d486
Jeff Britts <jbritts@adobe.com>
2021-09-15 09:42:23 -0400

    added auto

---
+++ sh-scripts/add-auto.sh
@@ -0,0 +1,9 @@
+# shellcheck shell=bash
+: || source lib.sh # trick shellcheck into finding certain referenced vars
+
+msg "Adding new grocery vertical ..."
+
+$cmd_prefix "
+  php $app_dir/bin/magento gxd:datainstall MagentoEse_VerticalDataAuto
+"
+msg "Auto website available at "$base_url"auto";

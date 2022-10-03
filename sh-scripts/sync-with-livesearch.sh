bbb77d855118a8371d997ad7404a0f03a9f413ac
Keith Bentrup <keith@keithbentrup.com>
2022-10-03 15:05:16 -0400

    fix for livesearch enabling

--- sh-scripts/sync-with-livesearch.sh
+++ sh-scripts/sync-with-livesearch.sh
@@ -13,16 +13,16 @@ git merge --abort || :
 git merge --strategy-option theirs "origin/$("$cli_path" environment:info -p "$project" -e "$environment" parent)"
 
 if "$ls_enabled"; then
-    perl -i -pe "s/'Magento_LiveSearch' => 1/'Magento_LiveSearch' => 0/" "$config_file"
-    perl -i -pe "s/'Magento_LiveSearchAdapter' => 1/'Magento_LiveSearchAdapter' => 0/" "$config_file"
-    perl -i -pe "s/'Magento_LiveSearchStorefrontPopover' => 1/'Magento_LiveSearchStorefrontPopover' => 0/" "$config_file"
-    perl -i -pe "s/'Magento_LiveSearchMetrics' => 1/'Magento_LiveSearchMetrics' => 0/" "$config_file"
-    perl -i -pe "s/'Magento_LiveSearchTerms' => 1/'Magento_LiveSearchTerms' => 0/" "$config_file"
-    perl -i -pe "s/'Magento_Elasticsearch' => 0/'Magento_Elasticsearch' => 1/" "$config_file"
-    perl -i -pe "s/'Magento_Elasticsearch6' => 0/'Magento_Elasticsearch6' => 1/" "$config_file"
-    perl -i -pe "s/'Magento_Elasticsearch7' => 0/'Magento_Elasticsearch7' => 1/" "$config_file"
-    perl -i -pe "s/'Magento_ElasticsearchCatalogPermissions' => 0/'Magento_ElasticsearchCatalogPermissions' => 1/" "$config_file"
-    perl -i -pe "s/'Magento_InventoryElasticsearch' => 0/'Magento_InventoryElasticsearch' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_LiveSearch' => 0/'Magento_LiveSearch' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_LiveSearchAdapter' => 0/'Magento_LiveSearchAdapter' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_LiveSearchStorefrontPopover' => 0/'Magento_LiveSearchStorefrontPopover' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_LiveSearchMetrics' => 0/'Magento_LiveSearchMetrics' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_LiveSearchTerms' => 0/'Magento_LiveSearchTerms' => 1/" "$config_file"
+  perl -i -pe "s/'Magento_Elasticsearch' => 1/'Magento_Elasticsearch' => 0/" "$config_file"
+  perl -i -pe "s/'Magento_Elasticsearch6' => 1/'Magento_Elasticsearch6' => 0/" "$config_file"
+  perl -i -pe "s/'Magento_Elasticsearch7' => 1/'Magento_Elasticsearch7' => 0/" "$config_file"
+  perl -i -pe "s/'Magento_ElasticsearchCatalogPermissions' => 1/'Magento_ElasticsearchCatalogPermissions' => 0/" "$config_file"
+  perl -i -pe "s/'Magento_InventoryElasticsearch' => 1/'Magento_InventoryElasticsearch' => 0/" "$config_file"
 fi
 
 git config user.email "chrome-extension@email.com"

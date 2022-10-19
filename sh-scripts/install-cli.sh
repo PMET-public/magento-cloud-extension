933034690c2a227e3f13683beaf73a0b745d5da0
Keith Bentrup <keith@keithbentrup.com>
2022-10-19 10:33:18 -0400

    separate check for homebrew and php

--- sh-scripts/install-cli.sh
+++ sh-scripts/install-cli.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+# if brew is missing, install it
+[[ -z "$(which brew)" ]] && {
+  printf "\n\033[0;32mInstalling Homebrew ...\033[0m\n\n"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  [[ "$0" = "zsh" ]] && {
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  }
+}
+
 # if php is missing (removed in OSX Monterey), install php via homebrew
 # https://developer.apple.com/forums/thread/681907
 [[ -z "$(which php)" ]] && {
-  [[ -z "$(which brew)" ]] && {
-    printf "\n\033[0;32mInstalling Homebrew ...\033[0m\n\n"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    [[ "$0" = "zsh" ]] && {
-      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-    }
-  }
   printf "\n\033[0;32mInstalling php ...\033[0m\n\n"
   brew install php@8.1
   printf "\n\033[0;32mLinking php ...\033[0m\n\n"

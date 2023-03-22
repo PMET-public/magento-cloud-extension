feca2cce4fba2f55d1b15004f545cec2985909f8
Keith Bentrup <keith@keithbentrup.com>
2023-03-21 20:23:34 -0400

    don't run paradoxlabs account updater cron job in debug mode

--- sh-scripts/debug-cron.sh
+++ sh-scripts/debug-cron.sh
@@ -3,7 +3,12 @@
 
 # run with -tt for color output
 $cmd_prefix -tt '
-  for i in $(vendor/bin/mr sys:cron:list --format=csv | grep -v -E "^cron_test_job" | sed "1d;s/,.*//"); do
+  for i in $(
+    vendor/bin/mr sys:cron:list --format=csv |
+    grep -v -E "^cron_test_job" |
+    grep -v -E "^paradoxlabs_authnetcim_account_updater" |
+    sed "1d;s/,.*//" |
+  ); do
     echo "Running: vendor/bin/mr sys:cron:run $i"
     vendor/bin/mr sys:cron:run $i
   done
